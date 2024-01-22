97956628a464074a8a66df8569afefb13b3919e1
berkekerey <150926719+berkekerey@users.noreply.github.com>
2024-01-22 19:07:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+import random
+n = random.randrange(1,50)
+guess = int(input("Write any number: "))
+while n!= guess:
+    if guess < n:
+        print("Too low")
+        guess = int(input("Write number again: "))
+    elif guess > n:
+        print("Too high!")
+        guess = int(input("Write number again: "))
+    else:
+      break
+print("the answer is correct!!")
